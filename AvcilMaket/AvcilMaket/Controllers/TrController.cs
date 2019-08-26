c1a045953505c5773c12fac8b91453defe6e5e66
b140910052@sakarya.edu.tr <b140910052@sakarya.edu.tr>
2019-08-26 22:04:36 +0300

    update

---
+++ AvcilMaket/AvcilMaket/Controllers/TrController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvcilMaket.Controllers
+{
+    public class TrController : Controller
+    {
+        // GET: Tr
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ AvcilMaket/AvcilMaket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AvcilMaket.MvcApplication" Language="C#" %>
